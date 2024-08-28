00c34fce488868664306953794b415b8c37df460
BSI-IT-UDOM <it@bsi.com.kh>
2024-08-28 14:46:05 +0700

    Update edit-supplier-popup.blade.php

--- resources/views/popups/edit-supplier-popup.blade.php
+++ resources/views/popups/edit-supplier-popup.blade.php
@@ -4,20 +4,20 @@
     @csrf
     @method('PATCH')
     <div class="bg-gradient-to-b from-blue-500 to-blue-400 rounded-t-lg px-6 py-4">
-      <h2 class="text-2xl font-bold text-white mb-2">Edit Supplier</h2>
+      <h2 class="text-2xl font-bold text-white mb-2">EDIT SUPPLIER</h2>
     </div>
     <div class="p-6">
       <input type="hidden" id="editSupId" name="Sup_id" value="{{ $data->Sup_id }}">
       <div class="mb-4">
-        <label for="editSupName" class="block text-sm font-medium text-gray-900">Supplier Name</label>
+        <label for="editSupName" class="block text-sm font-medium text-gray-900">NAME</label>
         <input type="text" id="editSupName" name="Sup_name" value="{{ $data->Sup_name }}" class="mt-1 p-2 border border-gray-300 rounded-md w-full">
       </div>
       <div class="mb-4">
-        <label for="editSupContact" class="block text-sm font-medium text-gray-900">Supplier Contact</label>
+        <label for="editSupContact" class="block text-sm font-medium text-gray-900">CONTACCT</label>
         <input type="text" id="editSupContact" name="Sup_contact" value="{{ $data->Sup_contact }}" class="mt-1 p-2 border border-gray-300 rounded-md w-full">
       </div>
       <div class="mb-4">
-        <label for="editSupAddress" class="block text-sm font-medium text-gray-900">Supplier Address</label>
+        <label for="editSupAddress" class="block text-sm font-medium text-gray-900">ADDRESS</label>
         <input type="text" id="editSupAddress" name="Sup_address" value="{{ $data->Sup_address }}" class="mt-1 p-2 border border-gray-300 rounded-md w-full">
       </div>
       <div class="flex justify-end">
