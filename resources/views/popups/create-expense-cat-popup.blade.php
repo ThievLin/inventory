567a707e608aee0b73dc28cc66b1542539d01058
BSI-IT-UDOM <it@bsi.com.kh>
2024-08-28 14:19:32 +0700

    Update create-expense-cat-popup.blade.php

--- resources/views/popups/create-expense-cat-popup.blade.php
+++ resources/views/popups/create-expense-cat-popup.blade.php
@@ -2,16 +2,16 @@
 <div id="createExpenseCatPopup" class="fixed inset-0 bg-black bg-opacity-60 flex justify-center items-center hidden z-60">
     <div class="bg-white rounded-lg shadow-lg max-w-xl w-full max-h-screen overflow-y-auto">
         <div class="bg-gradient-to-b from-blue-500 to-blue-400 rounded-t-lg px-6 py-4">
-            <h2 class="text-2xl font-bold text-white mb-2">Create new Expense Category</h2>
+            <h2 class="text-2xl font-bold text-white mb-2">NEW EXPENSE CATEGORY</h2>
         </div>
         <form id="itemCatForm" action="{{ route('setting.expense_cate') }}" method="POST" enctype="multipart/form-data" class="p-6">
             @csrf
             <div class="mb-4">
-                <label for="IEC_Khname" class="block text-sm font-medium text-gray-900 mb-1">Expense Cate Khname</label>
+                <label for="IEC_Khname" class="block text-sm font-medium text-gray-900 mb-1">NAME IN KHMER</label>
                 <input type="text" id="IEC_Khname" name="IEC_Khname" class="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500" required>
             </div>
             <div class="mb-4">
-                <label for="IEC_Engname" class="block text-sm font-medium text-gray-900 mb-1">Expense Cate Enname</label>
+                <label for="IEC_Engname" class="block text-sm font-medium text-gray-900 mb-1">NAME IN ENGLISH</label>
                 <input type="text" id="IEC_Engname" name="IEC_Engname" class="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500" required>
             </div>
             <div class="text-end">
