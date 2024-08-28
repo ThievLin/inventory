a001bad368d4d2dd0c9bf1ea22b6b8ee4c483bc5
BSI-IT-UDOM <it@bsi.com.kh>
2024-08-28 14:22:41 +0700

    Update create-location-popup.blade.php

--- resources/views/popups/create-location-popup.blade.php
+++ resources/views/popups/create-location-popup.blade.php
@@ -5,19 +5,20 @@
 <div class="create-popup-location hidden fixed inset-0 flex items-center justify-center z-50">
     <div class="bg-white rounded-lg shadow-lg w-[600px] max-h-screen overflow-y-auto">
         <div class="bg-gradient-to-b from-blue-500 to-blue-400 rounded-t-lg px-6 py-4">
-            <h2 class="text-2xl font-bold text-white">Create location</h2>
+            <h2 class="text-2xl font-bold text-white">NEW LOCATION</h2>
         </div>
         <form  action="{{ route('setting.location') }}" method="POST" enctype="multipart/form-data" id="create-location-form" class="space-y-4 px-6 py-2">
             @csrf
             <div class="p-2 w-full text-start">
                 <div class="mb-4">
-                    <label for="L_address" class="block mb-1">Address:</label>
+                    <label for="L_address" class="block mb-1">ADDRESS:</label>
                     <textarea type="text" id="L_address" name="L_address" class="w-full border border-gray-300 rounded-md p-2"></textarea>
                 </div>
             </div>
             <div class="p-2 mb-6">
-                <label for="S_id" class="block text-sm font-medium text-gray-900 mb-1">Shop Category</label>
+                <label for="S_id" class="block text-sm font-medium text-gray-900 mb-1">SHOP</label>
                 <select id="S_id" name="S_id" class="text-sm sm:text-sm font-medium border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500" required>
+                    <option value="">-- SHOP --</option>
                     @foreach ($shop_se as $data)
                     <option value="{{ $data->S_id }}">
                         {{ $data->S_name }}
