1de51612c6112b66ea6544d0e306cb3edc5e8569
BSI-IT-UDOM <it@bsi.com.kh>
2024-08-28 14:43:40 +0700

    Update edit-product-popup.blade.php

--- resources/views/popups/edit-product-popup.blade.php
+++ resources/views/popups/edit-product-popup.blade.php
@@ -2,7 +2,7 @@
 <div id="editPopup" class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 hidden">
     <div class="bg-white rounded-lg w-1/2">
         <div class="bg-gradient-to-b from-blue-500 to-blue-400 rounded-t-lg px-6 py-4">
-            <h2 class="text-2xl font-bold text-white mb-2">Edit Product</h2>
+            <h2 class="text-2xl font-bold text-white mb-2">EDIT PRODUCT</h2>
         </div>
    
         <form id="editProductPopup"  action="" method="POST" enctype="multipart/form-data" class="p-6">
@@ -10,17 +10,17 @@
             @method('PATCH')
             <input type="hidden" id="editPro_id" name="Pro_id" value="{{ $data->Pro_id }}">
             <div class="mb-4">
-                <label for="editPro_name_eng" class="block text-sm font-medium text-gray-900 mb-1">Product Name Eng</label>
+                <label for="editPro_name_eng" class="block text-sm font-medium text-gray-900 mb-1">NAME IN ENGLISH</label>
                 <input type="text" id="editPro_name_eng" name="Pro_name_eng" class="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500" required>
             </div>
             <div class="mb-4">
-                <label for="editPro_name_kh" class="block text-sm font-medium text-gray-900 mb-1">Product Name Kh</label>
+                <label for="editPro_name_kh" class="block text-sm font-medium text-gray-900 mb-1">NAME IN KHMER</label>
                 <input type="text" id="editPro_name_kh" name="Pro_name_kh" class="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500" required>
             </div>
             <div class="mb-6">
-                <label for="editCate_Khname" class="block text-sm font-medium text-gray-900 mb-1">Unit of Measure</label>
+                <label for="editCate_Khname" class="block text-sm font-medium text-gray-900 mb-1">CATEGORY</label>
                 <select id="editCate_Khname" name="Pro_Cate_id" class="text-sm sm:text-sm font-medium border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500" required>
-                    <option value="">Select product category</option>
+                    <option value="">-- CATEGORY --</option>
                     @foreach ($proCate as $data)
                     <option value="{{ $data->Pro_Cate_id }}" data-uom-name="{{ $data->Cate_Khname }}">
                         {{ $data->Cate_Khname }}
@@ -29,9 +29,9 @@
                 </select>
             </div>
             <div class="mb-4">
-                <label for="editimage" class="block text-sm font-medium text-gray-900 mb-1">Item Image</label>
+                <label for="editimage" class="block text-sm font-medium text-gray-900 mb-1">IMAGE</label>
                 <div>
-                    <button type="button" class="select-logo" onclick="document.getElementById('editimage').click()">Browse</button>
+                    <button type="button" class="select-logo" onclick="document.getElementById('editimage').click()">BROWSE</button>
                     <input type="file" id="editimage" name="image" style="display:none">
                 </div>
                 <img id="imagePreview" src="" alt="Image Preview" class="h-20 w-24 rounded hidden mt-2">
