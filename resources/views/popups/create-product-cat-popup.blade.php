dd3af89f2000bc903e963d833fe41d65ee10fd57
BSI-IT-UDOM <it@bsi.com.kh>
2024-08-28 14:29:50 +0700

    Update create-product-cat-popup.blade.php

--- resources/views/popups/create-product-cat-popup.blade.php
+++ resources/views/popups/create-product-cat-popup.blade.php
@@ -2,21 +2,22 @@
 <div id="createProductCatPopup" class="fixed inset-0 bg-black bg-opacity-60 flex justify-center items-center hidden z-60">
     <div class="bg-white rounded-lg shadow-lg max-w-xl w-full max-h-screen overflow-y-auto">
         <div class="bg-gradient-to-b from-blue-500 to-blue-400 rounded-t-lg px-6 py-4">
-            <h2 class="text-2xl font-bold text-white mb-2">Create new Product Category</h2>
+            <h2 class="text-2xl font-bold text-white mb-2">NEW PRODUCT CATEGORY</h2>
         </div>
         <form id="itemCatForm" action="{{ route('setting.product_cate') }}" method="POST" enctype="multipart/form-data" class="p-6">
             @csrf
             <div class="mb-4">
-                <label for="Cate_Khname" class="block text-sm font-medium text-gray-900 mb-1">Product Cate Khname</label>
+                <label for="Cate_Khname" class="block text-sm font-medium text-gray-900 mb-1">NAME IN KHMER</label>
                 <input type="text" id="Cate_Khname" name="Cate_Khname" class="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500" required>
             </div>
             <div class="mb-4">
-                <label for="Cate_Engname" class="block text-sm font-medium text-gray-900 mb-1">Product Cate Enname</label>
+                <label for="Cate_Engname" class="block text-sm font-medium text-gray-900 mb-1">NAME IN ENGLISH</label>
                 <input type="text" id="Cate_Engname" name="Cate_Engname" class="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500" required>
             </div>
             <div class="mb-6">
-                <label for="IPG_id" class="block text-sm font-medium text-gray-900 mb-1">Select Product Group</label>
+                <label for="IPG_id" class="block text-sm font-medium text-gray-900 mb-1">GROUP</label>
                 <select id="IPG_id" name="IPG_id" class="text-sm sm:text-sm font-medium border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500" required>
+                    <option value="">-- GROUP --</option>
                     @foreach ($group as $data)
                     <option value="{{ $data->IPG_id }}">
                         {{ $data->IPG_Khname }}
