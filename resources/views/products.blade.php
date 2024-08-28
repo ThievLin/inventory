d4ae372064395f7fa4dca6a45626d9c6eddc9452
BSI-IT-UDOM <it@bsi.com.kh>
2024-08-28 14:16:22 +0700

    Update products.blade.php

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -21,27 +21,27 @@
           <table class="min-w-full bg-white border-collapse">
             <thead>
               <tr class="bg-primary text-primary-foreground text-lg">
-                <th class="py-4 px-4 border border-white">Pro ID</th>
+                <th class="py-4 px-4 border border-white">NO.</th>
                 <th class="py-4 px-4 border border-white">
                   <a href="{{ url('/products?sortColumn=Pro_name_eng&sortOrder=' . (request('sortOrder') == 'asc' ? 'desc' : 'asc') . '&search=' . $searchTerm) }}">
-                    Pro Name Eng
+                    ENGLISH NAME
                     <i class="fas fa-xs {{ request('sortColumn') == 'Pro_name_eng' ? (request('sortOrder') == 'asc' ? 'fa-sort-alpha-up' : 'fa-sort-alpha-down') : 'fa-sort-alpha-down' }}"></i>
                   </a>
                 </th>
                 <th class="py-4 px-4 border border-white">
                   <a href="{{ url('/products?sortColumn=Pro_name_kh&sortOrder=' . (request('sortOrder') == 'asc' ? 'desc' : 'asc') . '&search=' . $searchTerm) }}">
-                    Pro Name Kh
+                    KHMER NAME
                     <i class="fas fa-xs {{ request('sortColumn') == 'Pro_name_kh' ? (request('sortOrder') == 'asc' ? 'fa-sort-alpha-up' : 'fa-sort-alpha-down') : 'fa-sort-alpha-down' }}"></i>
                   </a>
                 </th>
                 <th class="py-4 px-4 border border-white">
                   <a href="{{ url('/products?sortColumn=category&sortOrder=' . (request('sortOrder') == 'asc' ? 'desc' : 'asc') . '&search=' . $searchTerm) }}">
-                    Pro Category
+                    CATEGORY
                     <i class="fas fa-xs {{ request('sortColumn') == 'category' ? (request('sortOrder') == 'asc' ? 'fa-sort-alpha-up' : 'fa-sort-alpha-down') : 'fa-sort-alpha-down' }}"></i>
                   </a>
                 </th>
-                <th class="py-4 px-4 border border-white">Image</th>
-                <th class="py-4 px-4 border border-white">Action</th>
+                <th class="py-4 px-4 border border-white">IMAGE</th>
+                <th class="py-4 px-4 border border-white">ACTION</th>
               </tr>
             </thead>
             <tbody id="inventoryTableBody">
