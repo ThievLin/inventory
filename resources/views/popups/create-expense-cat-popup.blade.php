fe68b394a8069799ec7f2de974248ed04bba5b93
BSI-IT-UDOM <it@bsi.com.kh>
2024-08-29 14:57:48 +0700

    Update create-expense-cat-popup.blade.php

--- resources/views/popups/create-expense-cat-popup.blade.php
+++ resources/views/popups/create-expense-cat-popup.blade.php
@@ -15,8 +15,8 @@
                 <input type="text" id="IEC_Engname" name="IEC_Engname" class="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500" required>
             </div>
             <div class="text-end">
-                <button type="submit" class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">Save</button>
-                <button type="button" id="closeCreateExpenseCatPopup" class="bg-gray-300 hover:bg-gray-400 text-gray-900 px-4 py-2 rounded-md ml-2 focus:outline-none">Cancel</button>
+                <button type="submit" class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">SAVE</button>
+                <button type="button" id="closeCreateExpenseCatPopup" class="bg-gray-300 hover:bg-gray-400 text-gray-900 px-4 py-2 rounded-md ml-2 focus:outline-none">CANCEL</button>
             </div>
         </form>
     </div>
