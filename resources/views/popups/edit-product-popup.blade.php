e584e1e9a56afeff3247a05b655426eca39651fc
BSI-IT-UDOM <it@bsi.com.kh>
2024-08-29 15:05:06 +0700

    Update edit-product-popup.blade.php

--- resources/views/popups/edit-product-popup.blade.php
+++ resources/views/popups/edit-product-popup.blade.php
@@ -37,8 +37,8 @@
                 <img id="imagePreview" src="" alt="Image Preview" class="h-20 w-24 rounded hidden mt-2">
               </div>
             <div class="text-end">
-                <button type="submit" class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">Save Change</button>
-                <button type="button" id="cancelEdit" class="bg-gray-300 hover:bg-gray-400 text-gray-900 px-4 py-2 rounded-md ml-2 focus:outline-none">Cancel</button>
+                <button type="submit" class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">UPDATE</button>
+                <button type="button" id="cancelEdit" class="bg-gray-300 hover:bg-gray-400 text-gray-900 px-4 py-2 rounded-md ml-2 focus:outline-none">CANCEL</button>
             </div>
         </form>
     </div>
