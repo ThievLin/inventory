4508fabb859e398e0d05facd19f97fc7da934667
BSI-IT-UDOM <it@bsi.com.kh>
2024-08-29 15:06:01 +0700

    Update edit-shop-popup.blade.php

--- resources/views/popups/edit-shop-popup.blade.php
+++ resources/views/popups/edit-shop-popup.blade.php
@@ -36,8 +36,8 @@
                 </div>
             </div>            
             <div class="flex justify-end mt-3">
-                <button type="submit" class="bg-blue-500 text-white px-3 py-2 rounded-md hover:bg-blue-600 transition">Save Changes</button>
-                <button type="button" id="close-popup-shop" class="ml-2 bg-gray-300 text-gray-700 px-3 py-2 rounded-md hover:bg-gray-400 transition">Cancel</button>
+                <button type="submit" class="bg-blue-500 text-white px-3 py-2 rounded-md hover:bg-blue-600 transition">UPDATE</button>
+                <button type="button" id="close-popup-shop" class="ml-2 bg-gray-300 text-gray-700 px-3 py-2 rounded-md hover:bg-gray-400 transition">CANCEL</button>
             </div>
         </form>
     </div>
