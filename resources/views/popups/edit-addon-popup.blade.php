36f7d91b9bf66a97141b00de749813a286d1e59e
BSI-IT-UDOM <it@bsi.com.kh>
2024-08-29 15:03:16 +0700

    Update edit-addon-popup.blade.php

--- resources/views/popups/edit-addon-popup.blade.php
+++ resources/views/popups/edit-addon-popup.blade.php
@@ -32,8 +32,8 @@
             </div>
             
             <div class="text-end">
-                <button type="submit" class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">Save Changes</button>
-                <button type="button" id="cancelEdit" class="bg-gray-300 hover:bg-gray-400 text-gray-900 px-4 py-2 rounded-md ml-2 focus:outline-none">Cancel</button>
+                <button type="submit" class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">UPDATE</button>
+                <button type="button" id="cancelEdit" class="bg-gray-300 hover:bg-gray-400 text-gray-900 px-4 py-2 rounded-md ml-2 focus:outline-none">CANCEL</button>
             </div>
         </form>
     </div>
