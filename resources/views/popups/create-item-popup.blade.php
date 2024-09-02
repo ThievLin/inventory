819cabf005f43448acab39fb4ba9de3bea3420d4
BSI-IT-UDOM <it@bsi.com.kh>
2024-09-02 15:28:35 +0700

    Update create-item-popup.blade.php

--- resources/views/popups/create-item-popup.blade.php
+++ resources/views/popups/create-item-popup.blade.php
@@ -2,7 +2,7 @@
 <div id="popupItem" class="fixed inset-0 bg-black bg-opacity-60 flex justify-center items-center {{ $errors->any() ? '' : 'hidden' }} z-20">
     <div class="bg-white rounded-lg shadow-lg max-w-xl w-full max-h-screen overflow-y-auto">
         <div class="bg-gradient-to-b from-blue-500 to-blue-400 rounded-t-lg px-6 py-4">
-            <h2 class="text-2xl font-bold text-white mb-2">NEW ITEM</h2>
+            <h2 class="text-2xl font-bold text-white mb-2">NEW MATERIAL</h2>
         </div>
         <form id="itemForm" action="{{ route('items.store') }}" method="POST" enctype="multipart/form-data" class="p-6">
             @csrf
